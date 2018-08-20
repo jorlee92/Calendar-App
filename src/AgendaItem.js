53dab99390cb4af603b10b4e45dbe5b84fcc8ab2
jorlee92 <jorlee92@gmail.com>
2018-08-20 05:17:06 -0700

    Changed readme from the create-react-app default to a readme that describes the project

--- src/AgendaItem.js
+++ src/AgendaItem.js
@@ -2,27 +2,28 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class AgendaItem extends Component {
-    constructor(props){
-        super(props);
-        this.state = { 
-            message: "Undefined",
-        }
-    }
-    render() {
-        return (<article className="message">
-        <div className="message-header">
-          <p>{ this.props.message ? this.props.message.substring(0, 10) : "" + "...."}</p>
-          <button className="delete" aria-label="delete" onClick={() => this.props.deleteItem(this.props.id)}></button>
-        </div>
-        <div className="message-body">
-        {this.props.message}
-        <span className="is-pulled-right">{this.props.time}</span>
-        </div>
-      </article>
-      )
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      message: 'Undefined',
+    };
+  }
+
+  render() {
+    return (<article className="message">
+      <div className="message-header">
+            <p>{ this.props.message ? this.props.message.substring(0, 10) : '' + '....'}</p>
+            <button className="delete" aria-label="delete" onClick={() => this.props.deleteItem(this.props.id)} />
+          </div>
+      <div className="message-body">
+            {this.props.message}
+            <span className="is-pulled-right">{this.props.time}</span>
+          </div>
+            </article>
+    );
+  }
 }
 AgendaItem.propTypes = {
-    deleteItem: PropTypes.func
-  };
+  deleteItem: PropTypes.func,
+};
 export default AgendaItem;
